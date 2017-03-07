599600ca2f07959eac4f4bd6c8e9ddf7fe2d7d3f
Enoch2k2 <enoch2k2@hotmail.com>
2017-03-07 12:31:23 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import React from 'react';
+import ReactDom from 'react';
+import { combineReducers } from './reducers/index';
+import { books, recommendedBooks } from './reducers/manage_books';
 
+const rootReducer = combineReducers(books, recommendedBooks);
