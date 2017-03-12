0b7045d27395b13c22d61f66197c8a76b69df4d5
Cernan Bernardo <cernanb@gmail.com>
2017-03-11 19:11:14 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import React from 'react';
+import ReactDom from 'react';
+import { createStore } from 'redux';
+import { rootReducer } from './reducers/reducers';
 
+const store = createStore(rootReducer)
