1eff11447a78a4d2ea607e7fd617c37c9521901a
cmiller36 <m.calliemiller@gmail.com>
2017-03-24 20:01:34 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import React from 'react';
+import ReactDom from 'react';
+import {combineReducers} from '.reducers';
+import {books, recommendedBooks} from './reducers';
 
+const rootReducer = combineReducers(books, recommendedBooks);
