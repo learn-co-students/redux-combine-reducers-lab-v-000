75c4c8799b04f11acddab46fba4e1719a2ba7375
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-05-02 14:47:11 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 
+import React from 'react';
+import { combineReducers } from 'redux';
+import {books, recommendedBooks} from './reducers'
+
+const rootReducer = combineReducers({books, recommendedBooks});
+
