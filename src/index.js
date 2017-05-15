ed458189e82fba088b70ee4fceb9a1832de18a93
Ann Lee <aplee29@users.noreply.github.com>
2017-05-14 22:55:22 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { combineReducers } from 'redux';
+import { combineReducers } from './reducers';
+import { books, recommendedBooks } from './reducers';
 
+const rootReducer = combineReducers({books, recommendedBooks});
