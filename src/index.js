7ee57486b23a38876231e468130aaac3cbd8f5a1
github username <jilustrisimo@users.noreply.github.com>
2017-06-08 15:38:47 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+import { combineReducers } from 'react-redux';
+//import {books, recommendedBooks} from './reducers'
 
+//const rootReducer = combineReducers({books, recommendedBooks})
+
+const App = () => <div>Hi this is crazy</div>
+
+render(
+  <App />,
+  document.getElementById('container')
+);
