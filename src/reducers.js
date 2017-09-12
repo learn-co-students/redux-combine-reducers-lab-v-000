7b2d88a07afead9edc21bd902e42396438088aa5
Enoch2k2 <enoch2k2@hotmail.com>
2017-09-12 14:39:44 -0500

    Done.

---
+++ src/reducers.js
@@ -0,0 +1,34 @@
+export const books = (state=[], action) => {
+  switch (action.type) {
+    case "ADD_BOOK":
+      return [].concat(state, action.payload);
+    case "REMOVE_BOOK":
+      let idx = state.indexOf(action.payload);
+      return [].concat(state.slice(0, idx), state.slice(idx + 1, state.length))
+    default:
+      return state;
+  }
+}
+
+export const recommendedBooks = (state=[], action) => {
+  switch (action.type) {
+    case "ADD_RECOMMENDED_BOOK":
+      return [].concat(state, action.payload);
+    case "REMOVE_RECOMMENDED_BOOK":
+      let idx = state.indexOf(action.payload);
+      return [].concat(state.slice(0, idx), state.slice(idx + 1, state.length));
+    default:
+      return state;
+  }
+}
+
+export const combineReducers = reducers => {
+  return (state={}, action) => {
+    return Object.keys(reducers).reduce(
+      (nextState, key) => {
+        nextState[key] = reducers[key](state[key], action);
+        return nextState;
+      }, {}
+    )
+  }
+}
