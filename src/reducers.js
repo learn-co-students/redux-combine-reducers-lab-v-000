918de76c2d3cec1bb92649943218aa90cd492c64
github username <github email address>
2017-09-28 09:59:42 -0400

    Done.

--- src/reducers.js
+++ src/reducers.js
@@ -26,7 +26,7 @@ export function combineReducers(reducers) {
     return (state = {}, action) => {
         return Object.keys(reducers).reduce(
             (nextState, key) => {
-                nextState[key] = reducers[key](state[key], action)
+                nextState[key] = reducers[key](state[key], action); 
                 return nextState
             }, {}
         )
