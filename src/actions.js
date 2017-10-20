3a596b9ba966ff1805f635ede81ae7df1335e55c
marcnjaramillo <marcn.jaramillo@gmail.com>
2017-10-20 14:07:29 -0700

    Done.

---
+++ src/actions.js
@@ -0,0 +1,37 @@
+import { books, recommendedBooks } from '../src/reducers';
+import {combineReducers} from 'redux';
+
+const rootReducer = combineReducers({books, recommendedBooks})
+
+export function addBook(book){
+  return {
+    type: "ADD_BOOK",
+    payload: book
+  }
+}
+
+
+export function addRecommendedBook(book){
+  return {
+    type: "ADD_RECOMMENDED_BOOK",
+    payload: book
+  }
+}
+
+
+
+export function removeBook(book){
+  return {
+    type: "REMOVE_BOOK",
+    payload: book
+  }
+}
+
+
+export function removeRecommendedBook(book){
+
+  return {
+    type: "REMOVE_RECOMMENDED_BOOK",
+    payload: book
+  }
+}
