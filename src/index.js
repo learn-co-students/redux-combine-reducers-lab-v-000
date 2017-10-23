5f873551899f7fd87c7fd07e2d241a3cdb452ab0
ChristopherJamesN <nady.christopher@gmail.com>
2017-10-23 12:51:25 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import App from './app';
 
+// we wrap store in a function for testing purposes
+export const configureStore = () => {
+  return createStore(manageUsers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+};
+
+let store = configureStore();
+
+ReactDOM.render(
+    <App store={store}/>,
+  document.getElementById('root')
+);
