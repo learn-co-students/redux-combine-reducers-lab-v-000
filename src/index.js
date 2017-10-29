174fe463424985800c0dc0c7bb2daf70639d0e85
curlywallst <nnoyes@comcast.net>
2017-10-29 11:14:17 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { combineReducers } from 'redux'
+import { books } from './reducers'
+import { recommendedBooks } from './reducers'
 
+const rootReducer = combineReducers({books, recommendedBooks})
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  // <Provider store={store}>
+  //   <App store={store}/>
+  // </Provider>,
+  document.getElementById('root')
+);
