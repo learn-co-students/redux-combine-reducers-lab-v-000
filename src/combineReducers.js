4a14a4852ba17b8d54bd0d3e3c2a4a786612b5ed
mikka <mikkanthrope@gmail.com>
2017-10-28 23:18:32 -0700

    Done.

--- src/combineReducers.js
+++ src/combineReducers.js
@@ -1,5 +1,5 @@
 export function combineReducers(reducers){
-  return (state = {}, action) => {
+  return (state = {books, recommendedBooks}, action) => {
     return Object.keys(reducers).reduce(
       (nextState, key)=>{
         nextState[key] = reducers[key](state[key], action);
