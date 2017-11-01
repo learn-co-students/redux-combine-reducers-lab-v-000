8b8a9be777fb74f4613e948b81111c7913d67620
dustin <dandersonsoftware@outlook.com>
2017-10-31 23:36:40 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import { createStore } from 'redux';
+import { combineReducers } from 'redux';
 
+const rootReducer = combineReducers({
+  books,
+  recommendedBooks
+})
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
