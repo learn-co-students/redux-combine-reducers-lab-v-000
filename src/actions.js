7b96331e08e521aac98fbb57ad6aa28ceb359004
gb23 <gregbenj@hotmail.com>
2018-01-12 09:45:01 -0700

    Done.

---
+++ src/actions.js
@@ -0,0 +1,30 @@
+//add new book to library
+// add book to favorites list
+
+export function addBook(book){
+  return {
+    type: "ADD_BOOK",
+    payload: book
+  }
+}
+
+export function addRecommendedBook(book){
+  return {
+    type: "ADD_RECOMMENDED_BOOK",
+    payload: book
+  }
+}
+
+export function removeBook(book){
+  return {
+    type: "REMOVE_BOOK",
+    payload: book
+  }
+}
+
+export function removeRecommendedBook(book){
+  return {
+    type: 'REMOVE_RECOMMENDED_BOOK',
+    payload: book
+  }
+}
