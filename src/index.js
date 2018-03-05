8bd90a5c7fb5a280a8f0b9a542295f6ed75c07f8
marielfrank <marielbeth@gmail.com>
2018-03-05 15:14:55 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { combineReducers } from 'redux'
+import { recommendedBooks, books } from './reducers';
 
+// function combineReducers(reducers) {
+//     return (state={}, action) => {
+//         return Object.keys(reducers).reduce((nextState, key) => {
+//             nextState[key] = reducers[key](state[key], action);
+//             return nextState
+//         }, {})
+//     };
+// };
+
+const rootReducer = combineReducers({books, recommendedBooks})
+
+export function configureStore(){
+  return createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+};
+
+ReactDOM.render(
+  <App store={configureStore()} />,
+  document.getElementById('root')
+);
