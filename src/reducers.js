2db3992af0adec700aaf230e9c50ac37912a7678
github username <github email address>
2018-03-21 19:54:38 -0700

    Done.

---
+++ src/reducers.js
@@ -0,0 +1,27 @@
+import { combineReducers } from 'redux'
+
+export function books(state = [], action){
+    switch (action.type) {
+        case "ADD_BOOK":
+            return [].concat(state, action.payload)
+        case "REMOVE_BOOK":
+            const index = state.indexOf(action.payload)
+            return [].concat(state.slice(0, index), state.slice(index + 1, state.length))
+        default:
+            return state
+    }
+}
+
+export function recommendedBooks(state=[], action){
+    switch (action.type) {
+        case 'ADD_RECOMMENDED_BOOK':
+            return [].concat(state, action.payload)
+        case 'REMOVE_RECOMMENDED_BOOK':
+            const index = state.indexOf(action.payload)
+            return [].concat(state.slice(0, index), state.slice(index + 1, state.length))
+        default:
+            return state
+    }
+}
+
+export const rootReducer = combineReducers({books, recommendedBooks})
