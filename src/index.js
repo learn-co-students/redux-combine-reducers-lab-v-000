babe0ce496fb3457f3ef82b404942c11e3f48893
Phoebe Lau <phoebelau7@gmail.com>
2018-05-22 23:12:25 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import { combineReducers } from 'redux';
 
+// export function combineReducers(reducers){
+//   return (state = {}, action) => {
+//     return Object.keys(reducers).reduce(
+//       (nextState, key)=>{
+//         nextState[key] = reducers[key](state[key], action);
+//         return nextState
+//       }, {}
+//     )
+//   }
+// }
+
+
+const rootReducer = combineReducers({books, recommendedBooks})
