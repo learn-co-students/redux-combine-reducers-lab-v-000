fa9c1059bd091d8d0d16d85d2044046fe627cfab
jennianelson <jennifernelson118@gmail.com>
2018-06-01 17:41:20 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-
+import { combineReducers } from 'redux';
+//Wouldn't I also need to import the reducers?
+import {books, recommendedBooks} from './reducers'
+ 
+const rootReducer = combineReducers({books, recommendedBooks})
