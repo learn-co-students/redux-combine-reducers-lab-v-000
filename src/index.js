6597885306531a62fa1aa114b10758fff0f10fa1
vlaunyc <vlau.nyc@gmail.com>
2018-06-02 17:24:15 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import { combineReducers } from 'redux';
+import { addBook, addRecommendedBook, removeBook, removeRecommendedBook }  from '../src/actions';
+import { books, recommendedBooks } from '../src/reducers';
 
+const rootReducer = combineReducers({
+  books,
+  recommendedBooks
+});
