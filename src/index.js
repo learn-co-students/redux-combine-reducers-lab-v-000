aec1a53f27ebad13e45123847ebddee7dd55cad4
jessiehuff <huffjd1@gmail.com>
2018-06-06 12:27:17 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import { combineReducers } from 'redux';
 
+const rootReducer = combineReducers({
+  books: books,
+  recommendedBooks: recommendedBooks
+});
+
+export const store = createStore(rootReducer);
