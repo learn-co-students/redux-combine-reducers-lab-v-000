3e7eb5eb044b22ed9f4af97d1fa92ea28b5dc5db
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-06-08 18:46:46 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+// import {combineReducers} from 'redux'
 
+export function combineReducers(reducers){
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce(
+      (nextState, key)=>{
+        nextState[key] = reducers[key](state[key], action);
+        return nextState
+      }, {}
+    )
+  }
+}
