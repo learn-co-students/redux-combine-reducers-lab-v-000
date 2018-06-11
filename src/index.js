8b9aac5af664a21177c75f57dd49ba37bc6f00d6
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-11 17:06:45 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import App from './App';
 
+const store = createStore(combinedReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+  <App store={store} />
+  document.getElementById('root')
+);
