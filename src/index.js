3175a89c15b8a307f35482dcc6e96e25574b96f9
Fachastorm <rodrigo.lodeiro@live.com>
2018-06-18 16:41:51 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import { combineReducers } from 'redux'
- 
+import { addBook, addRecommendedBook, removeBook, removeRecommendedBook } from '../src/actions';
+import { books, recommendedBooks } from '../src/reducers';
+
 const rootReducer = combineReducers({books, recommendedBooks})
