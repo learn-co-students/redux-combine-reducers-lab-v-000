ab52655f878344273ed411b80c2d722e867df176
saurookadook <maskiella@gmail.com>
2018-06-23 14:14:31 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
+// import manageBook from './reducers/manageBook';
+//
+// // we wrap store in a function for testing purposes
+// export const configureStore = () => {
+//   return createStore(manageBand, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// };
+//
+// let store = configureStore();
+//
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App store={store}/>
+//   </Provider>,
+//   document.getElementById('root')
+// );
 
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({books, recommendedBooks})
+
+
+
+
+// state layout
+// {
+//   books: books,
+//   recommendedBooks: recommendedBooks
+// }
+
+
+// similar to Redux's provided `combineReducers()` function
+// export function combineReducers(reducers) {
+//   return (state = {}, action) => {
+//     return Object.keys(reducers).reduce(
+//       (nextState, key) => {
+//         nextState[key] = reducers[key](state[key], action);
+//         return nextState
+//       }, {}
+//     )
+//   }
+// }
