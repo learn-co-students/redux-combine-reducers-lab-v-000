32a9ffc69111e2891383d16d49709fdfbf8d7cb9
Michaelangelo Yambao <michaelangelo.yambao@gmail.com>
2018-07-02 18:59:07 -0400

    Done.

--- src/reducers.js
+++ src/reducers.js
@@ -10,6 +10,7 @@ export function books(state = [], action){
   }
 }
  
+ 
 export function recommendedBooks(state = [], action){
   switch (action.type) {
   case "ADD_RECOMMENDED_BOOK":
@@ -19,16 +20,5 @@ export function recommendedBooks(state = [], action){
     return [].concat(state.slice(0, idx), state.slice(idx + 1, state.length))
   default:
     return state
-  }
-}
-
-export function combineReducers(reducers){
-  return (state = {}, action) => {
-    return Object.keys(reducers).reduce(
-      (nextState, key)=>{
-        nextState[key] = reducers[key](state[key], action);
-        return nextState
-      }, {}
-    )
   }
 }
