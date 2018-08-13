f1eaf734ab307502cf90eaf8b09e3fee837578ca
ritchey0713 <ritchey0713@gmail.com>
2018-08-12 21:47:38 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import { combineReducers } from 'redux'
+import books from './reducers/booksReducer'
+import recommendedBooks from './reducers/recommendedBooksReducer'
 
+const rootReducer = combineReducers({
+books,
+recommendedBooks
+})
