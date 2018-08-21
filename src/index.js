eac9ec21493adaaf536db10b1a1abe4e09152fde
AReid987 <read.musik@gmail.com>
2018-08-20 20:29:00 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import { combinedReducers } from 'redux';
+import { createStore } from 'redux';
 
+const rootReducer = combinedReducers({books, recommendedBooks})
+
+export default createStore(rootReducer);
